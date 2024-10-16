ba49b6bd248614af478b734d197df94efc38139f
brianmeritech <brianmeritech@gmail.com>
2024-10-16 11:00:29 +0800

    1. Add Get Current Voltage function

--- ShellPkg/Application/VC/MF.c
+++ ShellPkg/Application/VC/MF.c
@@ -52,6 +52,7 @@
 
 #define CHECK_CONNECTION_DONE           0x81
 #define GET_FW_VERSION_DONE             0x91
+#define GET_CUR_VOLTAGE_DONE         0x92
 #define GET_SLOT_COUNT_DONE             0x95
 #define GET_FAN_RPM_DONE                0x96
 #define SET_LED_STATUS_DONE             0xA4
@@ -344,7 +345,18 @@ GetOutVoltage(
 {
   EFI_STATUS Status = EFI_UNSUPPORTED;
 
+  InitTxPkt();
+  InitRxPkt();
+  gTxPkt[CMD_INDX] = CMD_GET_CUR_VOLTAGE;
+  gTxPkt[DAT1_INDX] = (UINT8)Channel;
+  SerialPortWrite(gTxPkt, SIZE_CMD_PACKET);
 
+  if (!EFI_ERROR(ReadUartData())) {
+    if (gRxPkt[CMD_INDX] == GET_CUR_VOLTAGE_DONE) {
+      *Vol = gRxPkt[3] | ((gRxPkt[4] << 8) & 0xFF00);
+      Status = EFI_SUCCESS;
+    }
+  }
 
   return Status;
 }
